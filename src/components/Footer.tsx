f7fb05e439b4d33d8ea439f4964b00244261ae2e
osamaqaseem39 <osama.qa.39@gmail.com>
2025-07-29 15:35:00 +0500

    Fix Dashboard

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -150,7 +150,7 @@ export default function Footer() {
                 <svg className="w-5 h-5 text-punjabac-brand-light flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                 </svg>
-                <span className="text-gray-300">Landline:<br/> +92-42-36305101,<br/>+92-42-36361648</span>
+                <span className="text-gray-300">Landline: 92-42-36305101, 92-42-36361648</span>
               </div>
               <div className="flex items-center space-x-3">
                 <svg className="w-5 h-5 text-punjabac-brand-light flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
